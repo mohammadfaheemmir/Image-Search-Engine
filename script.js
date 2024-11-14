a53165abfee20e6d9b060154ba8950425981817c
Faheem <138996989+mohammadfaheemmir@users.noreply.github.com>
2024-11-14 21:12:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-let imgBox = document.getElementById('imgBox');
+let searchBox = document.getElementById("search-box");
+let searchButtton = document.getElementById("search-button");
+let searchResultContainer = document.getElementById("search-result");
+let showMore = document.getElementById("show-more");
+const clientId = "h_AW4EXdqzkUVJtS-N9Kg1LGWB8zHlwUF35Sdt0LvCs";
+let page = 1;
+let keyword;
+searchButtton.addEventListener("click", function () {
+    page = 1;
+    keyword = searchBox.value.toLowerCase();
+    searchImages();
+    searchBox.value = ""
+});
 
-let qrImage = document.getElementById('qrImg');
+showMore.addEventListener("click", function () {
+    page++
 
-let qrText = document.getElementById('qrText');
+    searchImages();
+});
+function searchImages() {
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${clientId}&per_page=12`;
+    fetch(url)
+        .then(response => response.json())
+        .then((imagesData) => {
 
-function generateQR() {
-    if(qrText.value.length > 0) {
-        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-        imgBox.classList.add('show-img');
-    }else {
-        qrText.classList.add('error');
-        setTimeout(()=>{
-            qrText.classList.remove('error');
-        },1000)
-    }
+            if (page === 1) {
+                searchResultContainer.innerHTML = "";
+            }
+
+
+            imagesData.results.forEach((result) => {
+                const imgContainer = document.createElement('div');
+                let img = document.createElement("img");
+                img.src = result.urls.small;
+                searchResultContainer.appendChild(imgContainer);
+                const imageLink = document.createElement('a')
+                imageLink.href = result.links.html;
+                imageLink.target = "_blank"
+                let button = document.createElement('button')
+                button.innerHTML = "Download"
+                button.classList.add("btn")
+                imageLink.appendChild(button)
+
+                imgContainer.appendChild(img);
+                imgContainer.appendChild(imageLink);
+            })
+            if (imagesData.total_pages > page) {
+                showMore.classList.remove("hide");
+            } else {
+                showMore.classList.add("hide");
+            }
+        }
+        )
 }
